51f0f9dd19ce9c89f5ef6741de94facc5013eac1
LollyGolly <valentina_96@inbox.ru>
2024-02-20 02:56:16 +0500

    test2 added

--- src/test/java/ru/netology/web/OrderCardTest.java
+++ src/test/java/ru/netology/web/OrderCardTest.java
@@ -19,4 +19,16 @@ class OrderCardTest {
 
         $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldNotOrderCardWhenNameInvalid() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Ivan Ivanov");
+        form.$("[data-test-id=phone] input").setValue("+79154276543");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
