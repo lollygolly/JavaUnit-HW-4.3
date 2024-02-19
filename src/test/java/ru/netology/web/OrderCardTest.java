a0ca48679318abc3b78fa53bc550501cacbaac5d
LollyGolly <valentina_96@inbox.ru>
2024-02-20 03:22:48 +0500

    All tests were added

--- src/test/java/ru/netology/web/OrderCardTest.java
+++ src/test/java/ru/netology/web/OrderCardTest.java
@@ -31,4 +31,52 @@ class OrderCardTest {
 
         $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
+
+    @Test
+    void shouldNotOrderCardWhenNameIsEmpty() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79154276543");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotOrderCardWhenPhoneIsInvalid() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иван Иванов");
+        form.$("[data-test-id=phone] input").setValue("123456");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotOrderCardWhenPhoneIsEmpty() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иван Иванов");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotOrderCardWhenCheckboxIsEmpty() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иван Иванов");
+        form.$("[data-test-id=phone] input").setValue("+79119592303");
+        // form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+
+        $("[data-test-id=agreement].input_invalid").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
 }
